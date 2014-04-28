f315a75d67098e379313262d6637ed11fba304bd
Emil Juul Jacobsen <=>
2014-04-28 13:24:23 +0200

    tried to remove all quizapp traces

--- QuestionnaireApp/gen/dk/itu/smdp/group2/R.java
+++ QuestionnaireApp/gen/dk/itu/smdp/group2/R.java
@@ -8,38 +8,34 @@
 package dk.itu.smdp.group2;
 
 public final class R {
-    public static final class array {
-        public static final int question1=0x7f050000;
-        public static final int question2=0x7f050001;
-    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dpCalendar=0x7f070003;
-        public static final int list=0x7f070000;
-        public static final int spEditText=0x7f070013;
-        public static final int spInteger=0x7f07000d;
-        public static final int svChoiceOptions=0x7f070008;
-        public static final int svsChoiceLL=0x7f070009;
-        public static final int svsLinearLayout=0x7f070015;
-        public static final int tlMatrix=0x7f070011;
-        public static final int tpCalendar=0x7f070004;
-        public static final int tvCalendarDesc=0x7f070002;
-        public static final int tvCalendarTitle=0x7f070001;
-        public static final int tvChoiceDesc=0x7f070006;
-        public static final int tvChoiceSelec=0x7f070007;
-        public static final int tvChoiceTitle=0x7f070005;
-        public static final int tvHeadingTitle=0x7f07000a;
-        public static final int tvIntegerDesc=0x7f07000c;
-        public static final int tvIntegerTitle=0x7f07000b;
-        public static final int tvMatrixDesc=0x7f07000f;
-        public static final int tvMatrixSelec=0x7f070010;
-        public static final int tvMatrixTitle=0x7f07000e;
-        public static final int tvParaTitle=0x7f070012;
-        public static final int tvTitle=0x7f070014;
+        public static final int dpCalendar=0x7f060003;
+        public static final int list=0x7f060000;
+        public static final int spEditText=0x7f060013;
+        public static final int spInteger=0x7f06000d;
+        public static final int svChoiceOptions=0x7f060008;
+        public static final int svsChoiceLL=0x7f060009;
+        public static final int svsLinearLayout=0x7f060015;
+        public static final int tlMatrix=0x7f060011;
+        public static final int tpCalendar=0x7f060004;
+        public static final int tvCalendarDesc=0x7f060002;
+        public static final int tvCalendarTitle=0x7f060001;
+        public static final int tvChoiceDesc=0x7f060006;
+        public static final int tvChoiceSelec=0x7f060007;
+        public static final int tvChoiceTitle=0x7f060005;
+        public static final int tvHeadingTitle=0x7f06000a;
+        public static final int tvIntegerDesc=0x7f06000c;
+        public static final int tvIntegerTitle=0x7f06000b;
+        public static final int tvMatrixDesc=0x7f06000f;
+        public static final int tvMatrixSelec=0x7f060010;
+        public static final int tvMatrixTitle=0x7f06000e;
+        public static final int tvParaTitle=0x7f060012;
+        public static final int tvTitle=0x7f060014;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -77,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
